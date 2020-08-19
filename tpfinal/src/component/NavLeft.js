5ccedd63b6355ea215ff2561a11deab355f4185a
Felipe Vidal Soto <felipe.vidal@est.fi.uncoma.edu.ar>
2020-08-19 19:05:32 -0300

    Actualizacion de barra lateral

--- tpfinal/src/component/NavLeft.js
+++ tpfinal/src/component/NavLeft.js
@@ -11,52 +11,10 @@ export default function Lista() {
           className="btn btn-outline-dark my-1 w-75 mx-auto btn-sm "
         >
           General
+          {/* Nombre categoria */}
         </button>
       </Link>
       {/*Aca termina el for*/}
-      <Link to="/categoria/:$id" className="link">
-        <button
-          type="button"
-          className="btn btn-outline-dark my-1 w-75 mx-auto btn-sm"
-        >
-          WTF
-        </button>
-      </Link>
-      <Link to="/categoria/:$id" className="link">
-        <button
-          type="button"
-          className="btn btn-outline-dark my-1 w-75 mx-auto btn-sm"
-        >
-          Deportes
-        </button>
-      </Link>
-
-      <Link to="/categoria/:$id" className="link">
-        <button
-          type="button"
-          className="btn btn-outline-dark my-1 w-75 mx-auto btn-sm "
-        >
-          Politica
-        </button>
-      </Link>
-
-      <Link to="/categoria/:$id" className="link">
-        <button
-          type="button"
-          className="btn btn-outline-dark my-1 w-75 mx-auto btn-sm"
-        >
-          Comidas
-        </button>
-      </Link>
-
-      <Link to="/categoria/:$id" className="link">
-        <button
-          type="button"
-          className="btn btn-outline-dark my-1 w-75 mx-auto btn-sm"
-        >
-          Random
-        </button>
-      </Link>
     </div>
   );
 }
