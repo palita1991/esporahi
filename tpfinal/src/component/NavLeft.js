efeada7adbede36d4a99b945b471fbca43e10fd4
Mariano Andres Palavecino <marianopalavecino7@gmail.com>
2020-08-19 23:28:38 -0300

    se instala react bootstrap para utilizar modal

--- tpfinal/src/component/NavLeft.js
+++ tpfinal/src/component/NavLeft.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { BrowserRouter as Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function Lista() {
+export default function NavLeft() {
   return (
-    <div class="btn-group-vertical d-flex justify-content-center  ">
+    <div className="btn-group-vertical d-flex justify-content-center">
       {/*Aca comienza el for*/}
-      <Link to="/categoria/:$id" className="link">
+      <Link to="/category/id" className="link w-100">
         <button
           type="button"
-          className="btn btn-outline-dark my-1 w-75 mx-auto btn-sm "
+          className="btn btn-outline-dark my-1 w-100 py-2 btn-sm"
         >
           General
           {/* Nombre categoria */}
--- tpfinal/src/component/NavTop.js
+++ tpfinal/src/component/NavTop.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { BrowserRouter as Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../img/logo_esporahi.png";
 import NavLeft from "./NavLeft";
+import Modal from "react-bootstrap/Modal";
 
 export default function NavTop() {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const showModal = () => {
+    setIsOpen(true);
+  };
+
+  const hideModal = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light border border-bottom  ">
-      <a className="navbar-brand" href="#">
-        <img src={logo} width="140" alt="" />
-      </a>
+    <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light border border-bottom navtop">
+      <Link to="/" className="link">
+        <div className="navbar-brand">
+          <img src={logo} width="140" alt="logo" />
+        </div>
+      </Link>
 
       <button
         className="navbar-toggler navbar-dark bg-dark"
@@ -23,53 +36,78 @@ export default function NavTop() {
       </button>
 
       <div className="collapse navbar-collapse" id="navbarToggler">
-        <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+        <ul className="navbar-nav ml-auto mt-2 mt-lg-0 text-center">
           <li className="nav-item active mx-2 my-1 d-none">
-            <a className="nav-link btn btn-outline-secondary" href="#">
-              +meme
-            </a>
+            <Link to="/create" className="link">
+              <button className="btn btn-outline-secondary py-2 btn-sm">
+                +meme
+              </button>
+            </Link>
           </li>
 
           <li className="nav-item active mx-2 my-1 d-none">
-            <a className="nav-link btn btn-outline-secondary" href="#">
-              usuario
-            </a>
+            <Link to="/profile" className="link">
+              <button className="btn btn-outline-secondary py-2 btn-sm">
+                profile
+              </button>
+            </Link>
           </li>
 
           <li className="nav-item active mx-2 my-1">
-            <a className="nav-link btn btn-outline-secondary" href="#">
+            <button
+              className="btn btn-outline-secondary py-2 btn-sm"
+              onClick={showModal}
+            >
               Log in
-            </a>
+            </button>
+            <Modal show={isOpen} onHide={hideModal} centered size="lg">
+              <Modal.Header closeButton>
+                <Modal.Title>Login</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>informacion</Modal.Body>
+              <Modal.Footer>boton</Modal.Footer>
+            </Modal>
           </li>
 
           <li className="nav-item active mx-2 my-1">
-            <a className="nav-link btn btn-outline-secondary" href="#">
+            <button
+              className="btn btn-outline-secondary py-2 btn-sm"
+              onClick={showModal}
+            >
               Sign up
-            </a>
+            </button>
+            <Modal show={isOpen} onHide={hideModal} centered size="lg">
+              <Modal.Header closeButton>
+                <Modal.Title>Register</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>informacion</Modal.Body>
+              <Modal.Footer>boton</Modal.Footer>
+            </Modal>
           </li>
 
           <li className="nav-item active mx-2 my-1 d-none">
-            <a className="nav-link btn btn-outline-secondary" href="#">
-              Log out
-            </a>
+            <Link to="/" className="link">
+              <button className="btn btn-outline-secondary py-2 btn-sm">
+                Log out
+              </button>
+            </Link>
           </li>
 
-          <Link to="/categoria/:$id" className="link">
+          <Link to="/categoria/:id" className="link">
             <div className=" d-lg-none d-xs-block " id="navbarToggler">
               <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle text-dark"
-                  href="#"
+                <button
+                  className="btn btn-outline-secondary dropdown-toggle py-2 btn-sm"
+                  type="button"
                   id="navbarDropdownMenuLink"
-                  role="button"
                   data-toggle="dropdown"
                   aria-haspopup="true"
                   aria-expanded="false"
                 >
                   Categorias
-                </a>
+                </button>
                 <div
-                  className="dropdown-menu"
+                  className="dropdown-menu text-center"
                   aria-labelledby="navbarDropdownMenuLink"
                 >
                   <NavLeft />
